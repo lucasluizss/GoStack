347f6d81beeaca99cad3aa758ca21bde9a561283
Lucas <lucasluizss@live.com>
2020-06-22 20:03:50 -0300

    Desafio 05: Primeiro projeto Node.js

---
+++ gostack-template-fundamentos-node/src/repositories/TransactionsRepository.ts
@@ -0,0 +1,57 @@
+import Transaction from '../models/Transaction';
+
+interface Balance {
+  income: number;
+  outcome: number;
+  total: number;
+}
+
+class TransactionsRepository {
+  private transactions: Transaction[];
+
+  constructor() {
+    this.transactions = [];
+  }
+
+  public all(): Transaction[] {
+    return this.transactions;
+  }
+
+  public getBalance(): Balance {
+    const { income, outcome } = this.transactions.reduce(
+      (accumulator: Balance, transaction: Transaction) => {
+        switch (transaction.type) {
+          case 'income':
+            accumulator.income += transaction.value;
+            break;
+          case 'outcome':
+            accumulator.outcome += transaction.value;
+            break;
+          default:
+            break;
+        }
+
+        return accumulator;
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    );
+
+    return {
+      income,
+      outcome,
+      total: income - outcome,
+    };
+  }
+
+  public create(transaction: Transaction): Transaction {
+    this.transactions.push(transaction);
+
+    return transaction;
+  }
+}
+
+export default TransactionsRepository;
